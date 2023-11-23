e299440b4f11d9c4c6a138362cf337c6c4858d92
amy-hgn <sandya1911@gmail.com>
2023-11-23 17:22:24 +0100

    TurnierModel erstellt

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config()
+
 const app = express()
 
 
@@ -9,9 +12,18 @@ app.get('/', (req, res) => {
 }) 
 
 app.get('/blog', (req, res) => {
-res.send('Hello Blog')
+res.send('Hello Blog my name is Amanda')
 })
 
-app.listen(3000, ()=> {
-console.log('Node API app is running on port 3000')
+mongoose.
+connect(process.env.DATABASE_URL)
+.then (() => {
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    })
+
+    console.log('connected to MongoDB')
+}).catch ((error) => {
+    console.log(error)
 })
+
