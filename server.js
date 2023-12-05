f38f4bde8e866179e8779987cd7e0e56a413e3a1
amy-hgn <sandya1911@gmail.com>
2023-12-05 14:19:11 +0100

    routing verbessern

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-const TurnierRouter = require('./src/app/turnier/turnierRoutes');
+const TurnierRouter = require('./turnierRoutes');
 
 const app = express();
 
