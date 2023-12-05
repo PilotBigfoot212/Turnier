21aa32280db4d91208518e2c68578665bd059c70
amy-hgn <sandya1911@gmail.com>
2023-12-05 14:49:15 +0100

    Port angepasst

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Global error handler middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
